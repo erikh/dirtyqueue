b55072d8cfab797ff1d0aea29bb81ccfc0454601
Erik Hollensbe <git@hollensbe.org>
2025-12-06 12:26:30 -0800

    thus ends the period of doc fixups; I'm going nuts here
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- dirtyqueue-derive/src/lib.rs
+++ dirtyqueue-derive/src/lib.rs
@@ -2,6 +2,8 @@ use proc_macro::TokenStream;
 use quote::quote;
 use syn::{DeriveInput, parse_macro_input};
 
+/// Deriving will add concrete implementations for select traits that are necessary for a struct
+/// derived in this way to be used as a DirtyQueue generic.
 #[proc_macro_derive(DirtyQueue)]
 pub fn derive(input: TokenStream) -> TokenStream {
 	let input = parse_macro_input!(input as DeriveInput);
